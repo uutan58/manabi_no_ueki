e024e6ccec0209181398546696beb3f5044e7017
uutan <58uutan92@gmail.com>
2024-04-18 08:50:25 +0900

    add:routesの設定、controllersにDeviseのメソッドを登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # /users/sign_up
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :phone_number, :full_name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "home/index"
+  root "home#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # get "up" => "rails/health#show", as: :rails_health_check
 end
