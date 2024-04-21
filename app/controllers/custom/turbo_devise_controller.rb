130d0e79e90eb5df831eb3772d1f7e71907f61d4
uutan <58uutan92@gmail.com>
2024-04-21 11:03:07 +0900

    fix:コードを修正

--- app/controllers/custom/turbo_devise_controller.rb
+++ app/controllers/custom/turbo_devise_controller.rb
@@ -1,18 +1,20 @@
-class TurboDeviseController < ApplicationController
-  class Responder < ActionController::Responder
-    def to_turbo_stream
-      controller.render(options.merge(formats: :html))
-    rescue ActionView::MissingTemplate => error
-      if get?
-        raise error
-      elsif has_errors? && default_action
-        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
-      else
-        redirect_to navigation_location
+module custom
+  class TurboDeviseController < ApplicationController
+    class Responder < ActionController::Responder
+      def to_turbo_stream
+        controller.render(options.merge(formats: :html))
+      rescue ActionView::MissingTemplate => error
+        if get?
+          raise error
+        elsif has_errors? && default_action
+          render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+        else
+          redirect_to navigation_location
+        end
       end
     end
-  end
 
-  self.responder = Responder
-  respond_to :html, :turbo_stream
+    self.responder = Responder
+    respond_to :html, :turbo_stream
+  end
 end
