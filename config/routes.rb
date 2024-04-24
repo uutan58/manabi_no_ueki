7681bdcdbb51f4bf0a4085b251a004e3a8f03374
uutan <58uutan92@gmail.com>
2024-04-24 10:53:15 +0900

    add:letter_opener_webのセットアップ

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # root "home#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
