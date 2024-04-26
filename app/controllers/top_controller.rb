7196e5989bab8470fdfe31970730e2cc2b64a7b7
uutan <58uutan92@gmail.com>
2024-04-26 16:25:43 +0900

    add:カレンダーの表示内容を変更

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,4 +2,47 @@ class TopController < ApplicationController
   def index
     @schedules = Schedule.all
   end
+
+  # def new
+  #   @schedule = Schedule.new
+  # end
+
+  # def show
+  #   @schedule = Schedule.find(params[:id])
+  # end
+
+  # def create
+  #   @schedule = Schedule.new(schedule_params)
+  #   if @schedule.save
+  #     redirect_to schedules_path, notice: "予定を作成しました。"
+  #   else
+  #     render 'new'
+  #   end
+  # end
+
+
+  # def destroy
+  #   @schedule = Schedule.find(params[:id])
+  #   @schedule.destroy
+  #   redirect_to schedule_path, notice:"削除しました"
+  # end
+
+  # def edit
+  #   @schedule = Schedule.find(params[:id])
+  # end
+
+  # def update
+  #   @schedule = Schedule.find(params[:id])
+  #   if @schedule.update(schedule_params)
+  #     redirect_to schedule_path, notice: "編集しました"
+  #   else
+  #     render 'edit'
+  #   end
+  # end
+
+  # private
+
+  # def schedule_params
+  #   params.require(:schedule).permit(:title, :content, :start_time)
+  # end
 end
