be95158d8c110586bc184f0a7b271bc05eef8731
uutan <58uutan92@gmail.com>
2024-05-01 15:41:51 +0900

    add:ログイン画面を作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
   get "home/index"
   root "staticpages#top"
-  # root "home#index"
+
+  resources :users
 
   get "up" => "rails/health#show", as: :rails_health_check
 
