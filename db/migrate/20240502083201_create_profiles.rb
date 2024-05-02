2c8ee38e3312c7efbb6af358bec724ddd9758ede
uutan <58uutan92@gmail.com>
2024-05-02 17:35:04 +0900

    add:profilesテーブルを作成

---
+++ db/migrate/20240502083201_create_profiles.rb
@@ -0,0 +1,10 @@
+class CreateProfiles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :profiles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.text :introduction
+
+      t.timestamps
+    end
+  end
+end
