61a458d320b02fc9085829174ace2d4fa55553d8
uutan <58uutan92@gmail.com>
2024-05-02 17:37:17 +0900

    update:ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root "staticpages#top"
   get "top/index"
   resources :schedules
