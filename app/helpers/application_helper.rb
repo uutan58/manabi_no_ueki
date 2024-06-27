c7519905a7d3a71aeb4f7ea4b2ac04f5deb6de84
uutan <58uutan92@gmail.com>
2024-06-27 12:10:08 +0900

    add:TO DO機能を作成。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def translate_status(status_key)
+    case status_key
+    when "未着手"
+      "未着手"
+    when "進行中"
+      "進行中"
+    when "完了"
+      "完了"
+    when "保留"
+      "保留"
+    else
+      "不明"
+    end
+  end
 end
